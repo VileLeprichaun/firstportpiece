df9f858fc1be7f842aaec1700569a1bae35f5420
Ryan <quillenryan@yahoo.com>
2021-08-04 21:43:32 -0400

    Added board

--- BattleShip.py
+++ BattleShip.py
@@ -1,1 +1,17 @@
 # BattleShip
+from random import randint
+import os
+
+board = []
+
+for i in range(0,9):
+    board.append(["0"]*9)
+print(board)
+
+
+#Board variables
+max_rows = 9
+max_column = 9 
+num_ships = 4
+max_ship_size = 5
+min_ship_size = 2
