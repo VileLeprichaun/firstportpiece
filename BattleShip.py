760fa570cda9d1f2f49f2e04b428d4a7730a5163
Ryan <quillenryan@yahoo.com>
2021-08-04 22:57:13 -0400

    First guess implimented

--- BattleShip.py
+++ BattleShip.py
@@ -4,8 +4,8 @@ from random import randint, random
 
 board = []
 
-for i in range(0,9):
-    board.append(["0"]*9)
+for i in range(0,5):
+    board.append(["0"]*5)
 
 def print_board(board):
     for row in board:
@@ -27,10 +27,16 @@ ship_col = random_col(board)
 guess_row = int(input("Guess Row: "))
 guess_col = int(input("Guess Col: "))
 
+print(ship_col)
+print(ship_row)
 
-#Board variables
-max_rows = 9
-max_column = 9 
-num_ships = 4
-max_ship_size = 5
-min_ship_size = 2
+
+if guess_row == ship_row and guess_col == ship_col:
+    print("Congratualtions! You sank my battleship!")
+else:
+    print("You missed my battleship")
+    board[guess_row][guess_col]= "X"
+    print_board(board)
+
+
+
