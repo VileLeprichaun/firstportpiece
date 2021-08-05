086e465de62f59f461ca22602ad3befcd1cb7f70
Ryan <quillenryan@yahoo.com>
2021-08-05 10:45:38 -0400

    finsihed game

--- BattleShip.py
+++ BattleShip.py
@@ -11,6 +11,7 @@ def print_board(board):
     for row in board:
         print (" ".join(row))
 
+print("Let's play Battleship")
 print_board(board)
 
 def random_row(board):
@@ -24,19 +25,29 @@ ship_row = random_row(board)
 ship_col = random_col(board)
 
 
-guess_row = int(input("Guess Row: "))
-guess_col = int(input("Guess Col: "))
+# print(ship_col)
+# print(ship_row)
+for turn in range(4):
+    print("Turn", turn+1)
 
-print(ship_col)
-print(ship_row)
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
 
-
-if guess_row == ship_row and guess_col == ship_col:
-    print("Congratualtions! You sank my battleship!")
-else:
-    print("You missed my battleship")
-    board[guess_row][guess_col]= "X"
-    print_board(board)
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratualtions! You sank my battleship!")
+        break
+    else:
+        if guess_row not in range(5) or guess_col not in range(5):
+            print("Opps, that's not even in the ocean")
+        elif board[guess_row][guess_col] == "X":
+            print("You've guessed that one already")
+        else:
+            print("You missed my battleship")
+            board[guess_row][guess_col]= "X"
+            if turn == 3:
+                print("Game Over")
+            
+        print_board(board)
 
 
 
