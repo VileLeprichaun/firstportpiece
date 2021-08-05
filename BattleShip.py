58e30fa2c93ee8ce58279d7bb8d8b8c91f512fa9
Ryan <quillenryan@yahoo.com>
2021-08-04 22:16:29 -0400

    board better, logic

--- BattleShip.py
+++ BattleShip.py
@@ -6,9 +6,26 @@ board = []
 
 for i in range(0,9):
     board.append(["0"]*9)
-print(board)
+
+def print_board(board):
+    for row in board:
+        print (" ".join(row))
+
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board)-1)
+def random_col(board):
+    return randint(0, len(board[0])-1)
 
 
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+
+guess_row =int()
+
+guess_row = int()
 #Board variables
 max_rows = 9
 max_column = 9 
